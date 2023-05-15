579b048f02c2299f002fb41c9f04e1c9013057c6
Oscar Miras <oscar.miras@gmail.com>
2023-05-15 18:31:44 +0000

    finished

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Product, Supplier, Order, OrderDetail
 #from models import Person
 
 app = Flask(__name__)
@@ -45,7 +45,82 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
+def create_product():
+    product = Product(product_name="Mandarina", list_price=1.1)
+    # encolo el producto que quiero crear en base de datos
+    db.session.add(product)
+    # Confirmo los cambios para que haga los INSERT
+    db.session.commit()
+    
+def get_product(id):
+    product = Product.query.get(id)
+    print(product)
+
+def get_all_products():
+    products = Product.query.all()
+    for p in products:
+        print(p)
+
+# productos que valen menos de 1.3 €
+def get_cheap_products():
+    # De todos los productos que me develve esta query filtrada, los quiero todos (me dará una lista)
+    products = Product.query.filter(Product.list_price < 1.3).all()
+    for p in products:
+        print(p)
+
+def get_all_products_of_a_single_supplier(id):
+    # Obtengo el supplier que tiene esta id
+    supplier = Supplier.query.get(id)
+
+    # Tengo una propiedad, de nombre 'products', que contiene todos los productos de los que soy proveedor
+    print(f'Productos para la compañia: {supplier.company_name}')
+    for p in supplier.products:
+        print(p)
+
+
+def insert_suppliers():
+    cocacola = Supplier(company_name='Coca Cola', homepage='https://cocacola.com')
+    db.session.add(cocacola)
+    fruitco = Supplier(company_name='Fruits Company', homepage='https://piñacolada.com')
+    db.session.add(fruitco)
+    db.session.commit()
+
+def create_order(city):
+    order = Order(ship_city=city)
+    db.session.add(order)
+    db.session.commit()
+
+# Añadir un producto a una orden de compra
+def add_to_cart(o_id, p_id):
+    # order_id es el nombre de la columna
+    # o_id simplemente es el valor del parámetro de la función
+    od = OrderDetail(order_id=o_id, product_id = p_id)
+    db.session.add(od)
+    db.session.commit()
+
+# Muestrame todos los productos de una orden de compra
+def get_products_from_order(o_id):
+    order = Order.query.get(o_id)
+    for detail in order.orderdetails:
+        print(detail)
+
+
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
+    # app.run(host='0.0.0.0', port=PORT, debug=False)
+
+    # Lo necesitamos para pdoer ejecutar código Python normal fuera del entorno de Flask
+    with app.app_context():
+        #create_product()
+        # get_product(2)
+        # get_all_products()
+        # get_cheap_products()
+        # insert_suppliers()
+        # get_all_products_of_a_single_supplier(2)
+        #add_to_cart(1, 1)
+        #add_to_cart(1, 5)
+        get_products_from_order(1)
+        
+
+
--- src/models.py
+++ src/models.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+
 
 db = SQLAlchemy()
 
@@ -17,3 +19,52 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # columna de nombre product_name, será un string de 120 carácteres como máximo y siempre que cree un producto tengo que informar de este valor
+    product_name = db.Column(db.String(120), nullable=False)
+
+    # el precio del producto
+    list_price = db.Column(db.Float, nullable=False)
+
+    #Todo producto es entrega por un proveedor (supplier)
+    # Nombre columna es arbitrario, pero por convencción, ponemos el nombre de la tabla que está relacionado_id
+    supplier_id = db.Column(db.Integer, db.ForeignKey('supplier.id'), nullable=True)
+
+    # Esto me va permitir navegar de este producto a su provedor. De esta manera, puedo obtener el nombre del provedor, su homepage, en general, cualquier columna de esa otra tabla
+    # backref : es un parámetro opcional pero recomendable que nos va a permitir hacer la negación inveersa. Desde Supplier, yo voy a poder navegar a los productos que dicho proveedor suministra. Siempre que usemos Flask, ponedlo.
+    # Esto es puramente algo de SQLAlchemy
+    supplier = relationship('Supplier', backref='products')
+
+
+
+    def __repr__(self):
+        return f'<Product {self.product_name} - {self.list_price} € y pertenece al proveedor {self.supplier.company_name}>'
+
+
+class Supplier(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    company_name =  db.Column(db.String(120),unique=True, nullable=False)
+    # Aún hay empresas sin página Web...
+    homepage = db.Column(db.String(120))
+
+    def __repr__(self):
+        return f'<Supplier {self.company_name}'
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    ship_city = db.Column(db.String(80), nullable=False)
+
+class OrderDetail(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+
+    # En backref ponemos el nombre de la tabla, en minúsculas y en plural
+    order = relationship('Order', backref='orderdetails')
+    product = relationship('Product', backref='orderdetails')
+
+    def __repr__(self):
+        return f'{self.product.product_name} - {self.order.ship_city}'
